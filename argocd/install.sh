14cbcc3716fc04e9a44f8a1d855af1f3660348b4
pierre-richelle <pierre.richelle@gmail.com>
2024-07-03 12:36:47 +0200

    kustomize

--- argocd/install.sh
+++ argocd/install.sh
@@ -1,3 +1,4 @@
 oc create ns openshift-gitops-operator
 oc apply -f ./config/operatorGroup.yaml
 oc apply -f ./config/subscription.yaml
+oc adm policy add-cluster-role-to-user cluster-admin -z openshift-gitops-argocd-application-controller -n openshift-gitops
